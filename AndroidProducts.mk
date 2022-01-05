c32621c73ec6ef6e1a69010f0fb598555b261336
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-01-05 16:12:53 +0100

    grandpplte: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 0 Manufacturer: samsung Model: SM-G532G Script version: 2.1.0 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_grandpplte.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_grandpplte-user \
+    omni_grandpplte-userdebug \
+    omni_grandpplte-eng
